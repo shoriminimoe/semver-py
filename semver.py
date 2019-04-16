87f74aa2b49618b105682696765947304b008fe6
Sam Caldwell <1113028@raytheon.com>
2019-04-16 16:58:37 -0700

    Add lt, eq, and bump functions
    Add corresponding tests

--- semver.py
+++ semver.py
@@ -23,7 +23,7 @@ class SemVer():
         self.parse(instr)
 
     def __str__(self):
-        versionString = '.'.join([self.major, self.minor, self.patch])
+        versionString = '.'.join([str(self.major), str(self.minor), str(self.patch)])
         if self.prerelease:
             versionString += '-' + self.prerelease
         if self.build:
@@ -36,15 +36,63 @@ class SemVer():
     def parse(self, instr):
         reMatch = self.semverRE.match(instr)
         self.prefix = reMatch.group('prefix')
-        self.major = reMatch.group('major')
-        self.minor = reMatch.group('minor')
-        self.patch = reMatch.group('patch')
+        self.major = int(reMatch.group('major'))
+        self.minor = int(reMatch.group('minor'))
+        self.patch = int(reMatch.group('patch'))
         self.prerelease = reMatch.group('prerelease')
         self.build = reMatch.group('build')
 
-    #TODO 
-    #def __lt__(self, other):
-    #def __eq__(self, other):
+    def __lt__(self, other):
+        if self.major < other.major:
+            return True
+        elif self.minor < other.minor:
+            return True
+        elif self.patch < other.patch:
+            return True
+        elif self.prerelease is None and other.prerelease is not None:
+            #TODO This prerelease comparison is not correct.
+            #TODO It needs to account for the possible dot-separated 
+            #TODO list of fields and also mark letters higher than numbers
+            return False
+        elif self.prerelease is not None and other.prerelease is None:
+            return True
+        elif self.prerelease < other.prerelease:
+            return True
+        else:
+            return False
+    
+    def __eq__(self, other):
+        if self.major == other.major \
+                and self.minor == other.minor \
+                and self.patch == other.patch \
+                and self.prerelease == other.prerelease:
+            return True
+        else:
+            return False
+        
+    def bump(self, partString):
+        if partString == 'major':
+            self.major += 1
+            self.minor = 0
+            self.patch = 0
+            self.prerelease = None
+            self.build = None
+        elif partString == 'minor':
+            self.minor += 1
+            self.patch = 0
+            self.prerelease = None
+            self.build = None
+        elif partString == 'patch':
+            self.patch += 1
+            self.prerelease = None
+            self.build = None
+        elif partString == 'prerelease' or partString == 'build':
+            return NotImplemented
+        else:
+            raise
+
+
+
     #def __hash__(self):
         #return hash((
             #self.prefix,
--- semver_test.py
+++ semver_test.py
@@ -8,33 +8,65 @@ class SemVerTest(unittest.TestCase):
     def setUp(self):
         self.sv1Str = 'v1.2.3-alpha.0+build123-42-g123abcd'
         self.sv1 = semver.SemVer(self.sv1Str)
-        self.sv2Str = 'v1.2.3-alpha.0+build123'
+        self.sv2Str = 'v1.2.3'
         self.sv2 = semver.SemVer(self.sv2Str)
+        self.sv3Str = 'v1.2.3-alpha.0+11-gabcd123'
+        self.sv3 = semver.SemVer(self.sv3Str)
+        self.sv4Str = 'v1.2.3-rc.1.0'
+        self.sv4 = semver.SemVer(self.sv4Str)
+        self.sv5Str = 'v1.2.3-rc.2.1'
+        self.sv5 = semver.SemVer(self.sv5Str)
 
     def testStr(self):
         self.assertEqual(str(self.sv1), self.sv1Str.lstrip('v'))
         self.assertEqual(str(self.sv2), self.sv2Str.lstrip('v'))
+    
     def testTag(self):
         self.assertEqual(self.sv1.tag(), self.sv1Str)
         self.assertEqual(self.sv2.tag(), self.sv2Str)
+    
     def testPrefix(self):
         self.assertEqual(self.sv1.prefix, 'v')
         self.assertEqual(self.sv2.prefix, 'v')
+    
     def testMajor(self):
-        self.assertEqual(self.sv1.major, '1')
-        self.assertEqual(self.sv2.major, '1')
+        self.assertEqual(self.sv1.major, 1)
+        self.assertEqual(self.sv2.major, 1)
+    
     def testMinor(self):
-        self.assertEqual(self.sv1.minor, '2')
-        self.assertEqual(self.sv2.minor, '2')
+        self.assertEqual(self.sv1.minor, 2)
+        self.assertEqual(self.sv2.minor, 2)
+    
     def testPatch(self):
-        self.assertEqual(self.sv1.patch, '3')
-        self.assertEqual(self.sv2.patch, '3')
+        self.assertEqual(self.sv1.patch, 3)
+        self.assertEqual(self.sv2.patch, 3)
+    
     def testPrerelease(self):
         self.assertEqual(self.sv1.prerelease, 'alpha.0')
-        self.assertEqual(self.sv2.prerelease, 'alpha.0')
+        self.assertIsNone(self.sv2.prerelease)
+    
     def testBuild(self):
         self.assertEqual(self.sv1.build, 'build123-42-g123abcd')
-        self.assertEqual(self.sv2.build, 'build123')
+        self.assertIsNone(self.sv2.build)
+    
+    def testLT(self):
+        self.assertTrue(self.sv1 < self.sv2)
+        self.assertFalse(self.sv1 < self.sv3)
+        self.assertTrue(self.sv1 < self.sv4)
+        self.assertTrue(self.sv1 < self.sv5)
+        self.assertTrue(self.sv4 < self.sv5)
+    
+    def testEQ(self):
+        self.assertTrue(self.sv1 == self.sv3)
+        self.assertFalse(self.sv1 == self.sv2)
+    
+    def testBump(self):
+        self.sv1.bump('major')
+        self.assertTrue(str(self.sv1), 'v2.0.0')
+        self.sv1.bump('minor')
+        self.assertTrue(str(self.sv1), 'v2.1.0')
+        self.sv1.bump('patch')
+        self.assertTrue(str(self.sv1), 'v2.1.1')
 
 if __name__ == '__main__':
     unittest.main()
